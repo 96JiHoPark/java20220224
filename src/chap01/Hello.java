4a0849c1a4dc3ad6e1da3cf8dc8ea3fa797f6f33
96JiHoPark <99157655+96JiHoPark@users.noreply.github.com>
2022-02-24 12:16:32 +0900

    first commit

---
+++ src/chap01/Hello.java
@@ -0,0 +1,14 @@
+package chap01;
+/*
+ * 작성자 : 박지호
+ * 작성일 : 2012.12.12
+ * */
+public class Hello {
+	// 프로그램 실행 진입점
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello, welcome to the java World!");
+		
+		// 단축키 : ctrl + /
+	}	// end of main
+}	// end of class
